107b680236ac9f689da1d3ac77b6c5d0abf02757
Harry Ji <jiharry@hotmail.com>
2021-10-25 11:33:02 -0700

    testing

--- spec/factories/ideas.rb
+++ spec/factories/ideas.rb
@@ -1,6 +1,7 @@
+RANDOM_HUNDRED_CHARS="hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world hello worldhello world"
 FactoryBot.define do
   factory :idea do
-    title { "MyString" }
-    description { "MyText" }
+    sequence(:title){ |n| Faker::Educator.university + "#{n}"}
+    description{ Faker::Marketing.buzzwords + "#{RANDOM_HUNDRED_CHARS}"}
   end
 end
